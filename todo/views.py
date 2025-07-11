2e71637dc6fd0aa10384947abb2156c2e470e523
Jodu Thirupathaiah <thirupathaiahjodu0421@gmail.com>
2025-07-11 10:56:22 +0530

    initial commit

---
+++ todo/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def index(request):
+    tasks = Task.objects.all()
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+            return redirect('index')
+    return render(request, 'index.html', {'tasks': tasks})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('index')
